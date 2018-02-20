7f286ea8256c5c8522d050367fc5b1aa40df8975
Lam Chi Thanh <lamchithanh1997@gmail.com>
2018-02-21 00:03:07 +0700

    Add random sampling active learning

--- mnist/mnist_active_learning.py
+++ mnist/mnist_active_learning.py
@@ -5,6 +5,29 @@ from tensorflow.examples.tutorials.mnist import input_data
 from mnist_bayesian_neural_network import MnistBayesianMultiLayer
 from bayesian_cnn import BayesianCNN
 
+def random_sample_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test, iters=50, k=100):
+    for i in range(iters):
+        print("Active learning iteration %d" % i)
+        print("Total data used so far: %d" % train_x.shape[0])
+        pred = np.zeros(unlabelled_y.shape)
+
+        idx = np.random.choice(len(unlabelled_x), k, replace=False)
+
+        # Add the selected data points to train set.
+        train_x = np.append(train_x, np.take(unlabelled_x, idx, axis=0), axis=0)
+        train_y = np.append(train_y, np.take(unlabelled_y, idx, axis=0), axis=0)
+
+        # Train the model again.
+        model.optimize(train_x, train_y)
+
+        # Delete the selected data points from the unlabelled set.
+        unlabelled_x = np.delete(unlabelled_x, idx, 0)
+        unlabelled_y = np.delete(unlabelled_y, idx, 0)
+
+        # Test the accuracy of the model.
+        acc = model.validate(x_test, y_test)
+        print(np.array(acc).mean())
+
 def maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test, iters=50, k=100):
     for i in range(iters):
         print("Active learning iteration %d" % i)
@@ -61,4 +84,5 @@ if __name__ == "__main__":
         train_y = y_train[:100]
         unlabelled_x = x_train[100:]
         unlabelled_y = y_train[100:]
-        maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
+        # maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
+        random_sample_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)

13c0f21ce42efee1ace95e03269583503e0b281a
Lam Chi Thanh <lamchithanh1997@gmail.com>
2018-03-01 00:44:11 +0800

    max entropy

--- mnist/mnist_active_learning.py
+++ mnist/mnist_active_learning.py
@@ -160,6 +160,6 @@ if __name__ == "__main__":
         train_y = y_train[:100]
         unlabelled_x = x_train[100:]
         unlabelled_y = y_train[100:]
-        # maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
-        maximum_meanvar_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
+        maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
+        # maximum_meanvar_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)
         # first_layer_maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test)

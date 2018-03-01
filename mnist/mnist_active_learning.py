379365916503688172106f07a8bb72282ad9b224
Lam Chi Thanh <lamchithanh1997@gmail.com>
2018-03-01 18:31:39 +0800

    bcnn

--- mnist/mnist_active_learning.py
+++ mnist/mnist_active_learning.py
@@ -30,7 +30,7 @@ def random_sample_active_learning(model, train_x, train_y, unlabelled_x, unlabel
         acc_mean = np.array(acc).mean()
         print(acc_mean)
         all_accuracy = np.append(all_accuracy, acc_mean)
-        np.save('./nn_random.npy', all_accuracy)
+        np.save('./cnn_random.npy', all_accuracy)
 
 def maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test, iters=10, k=100):
     all_accuracy = np.array([])
@@ -63,7 +63,7 @@ def maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlab
         acc_mean = np.array(acc).mean()
         print(acc_mean)
         all_accuracy = np.append(all_accuracy, acc_mean)
-        np.save('./nn_max_entropy.npy', all_accuracy)
+        np.save('./cnn_max_entropy.npy', all_accuracy)
 
 
 def maximum_meanvar_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test, iters=10, k=100):
@@ -97,7 +97,7 @@ def maximum_meanvar_active_learning(model, train_x, train_y, unlabelled_x, unlab
         print(acc_mean)
         all_accuracy = np.append(all_accuracy, acc_mean)
 
-    np.save('./nn_max_meanvar.npy', all_accuracy)
+    np.save('./cnn_max_meanvar.npy', all_accuracy)
 
 
 def first_layer_maximum_entropy_active_learning(model, train_x, train_y, unlabelled_x, unlabelled_y, x_test, y_test, iters=50, k=100):
@@ -151,8 +151,8 @@ def load_data():
     return (x_train, y_train, x_test, y_test)
 
 if __name__ == "__main__":
-    # model = BayesianCNN()
-    model = MnistBayesianSingleLayer()
+    # model = MnistBayesianSingleLayer()
+    model = BayesianCNN()
     print("Run active learning random.")
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
